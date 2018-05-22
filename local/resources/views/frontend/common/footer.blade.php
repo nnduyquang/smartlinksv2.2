0bc751821697ec4d1750385c4c495952058e9076
Quang-PC <nnduyquang@gmail.com>
2018-05-22 22:58:13 +0700

    ok

--- local/resources/views/frontend/common/footer.blade.php
+++ local/resources/views/frontend/common/footer.blade.php
@@ -1,45 +1,3 @@
-{{--<div id="footer2" class="col-md-12">--}}
-{{--<div class="row">--}}
-{{--<div class="container">--}}
-{{--<div class="row">--}}
-{{--<div class="backToTop footer__jumpTop"><a href="#"><i class="fa fa-chevron-up"></i></a></div>--}}
-{{--<div id="main-footer" class="col-md-12">--}}
-{{--<div class="row">--}}
-{{--<div id="main-footer-left" class="col-md-6">--}}
-{{--<div class="logo">--}}
-{{--{{ Html::image('images/logo/smartlinks-logo-footer.png','',array('class'=>'logo-footer')) }}--}}
-{{--</div>--}}
-{{--<div class="footer-info">--}}
-{{--<ul>--}}
-{{--<li><i class="fa fa-home" aria-hidden="true"></i>25 Hoàng Diệu, P.12, Quận 4,--}}
-{{--TPHCM--}}
-{{--</li>--}}
-{{--<li><i class="fa fa-phone-square" aria-hidden="true"></i>(028) 66 83 00 91 ---}}
-{{--Hotline: 0914.348.131--}}
-{{--</li>--}}
-{{--<li><i class="fa fa-envelope" aria-hidden="true"></i>info@smartlinks.vn</li>--}}
-{{--<li><p>Smartlinks đem lại hiệu quả cao cho khách hàng bằng cách sử dụng kỹ thuật--}}
-{{--công nghệ--}}
-{{--mới nhất trong lĩnh vực marketing online</p></li>--}}
-{{--</ul>--}}
-{{--</div>--}}
-
-{{--</div>--}}
-{{--<div id="main-footer-right" class="col-md-6">--}}
-{{--<div class="fb-page" data-href="https://www.facebook.com/smartlinks.vn" data-tabs="timeline"--}}
-{{--data-height="250" data-small-header="false" data-adapt-container-width="false"--}}
-{{--data-hide-cover="false" data-show-facepile="false">--}}
-{{--<blockquote cite="https://www.facebook.com/smartlinks.vn" class="fb-xfbml-parse-ignore">--}}
-{{--<a href="https://www.facebook.com/smartlinks.vn">SmartLinks Digital Marketing</a>--}}
-{{--</blockquote>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
 <div id="footer">
     <div class="container">
         <div class="row no-gutters">
@@ -55,17 +13,17 @@
                     <ul class="footer-social">
                         <li>
                             <a href="#">
-                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-linkedin-in"></i>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-twitter"></i>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-google-plus-g"></i>
                             </a>
                         </li>
                         <li>
@@ -83,24 +41,9 @@
                 <div class="box-footer no-border pl-5">
                     <h3>Services</h3>
                     <ul class="services">
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-angle-right"></i>Google Adwords</a>
-                        </li>
+                        @foreach($listMenu['categoryService'] as$key=>$data)
+                            <li><a href="{{URL::to('dich-vu/'.$data->path)}}"><i class="fas fa-angle-right"></i>{{$data->title}}</a></li>
+                        @endforeach
                     </ul>
                 </div>
             </div>
